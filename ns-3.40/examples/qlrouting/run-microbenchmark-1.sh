edf2c0a0fc2b163c775f75b306bf68a5be9fcf5c
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2026-02-20 15:19:10 +0100

    Fix scripts

--- ns-3.40/examples/qlrouting/run-microbenchmark-1.sh
+++ ns-3.40/examples/qlrouting/run-microbenchmark-1.sh
@@ -30,19 +30,5 @@ rm -rf results/${RESULTS_DIR}/qlr_1/0
 QLR_ACTIVE=1 P4_PROGRAM=examples/qlrouting/qlr_build/qlr.json P4_COMMANDS="examples/qlrouting/resources/" EXPERIMENT_NAME=${EXPERIMENT_NAME} CONGESTION_CONTROL=${CONGESTION_CONTROL} WORKLOAD_FILE=${WORKLOAD_FILE} EDGES="0,1;0,2;1,2" HOSTS="1,1,1" SWITCHES=3 DAGS="0:1-0,2-0,2-1;1:0-1,0-2,2-1;2:0-1,0-2,1-2" END=10 bash run_experiment.sh
 done
 
-
-# for WORKLOAD_FILE in \
-#     "examples/qlrouting/resources/3_nodes/workloads_central/wl1.csv" \
-#     "examples/qlrouting/resources/3_nodes/workloads_central/wl2.csv" \
-#     "examples/qlrouting/resources/3_nodes/workloads_central/wl3.csv" \
-#     "examples/qlrouting/resources/3_nodes/workloads_central/wl4.csv"
-# do
-# WORKLOAD_NAME="$(basename "$WORKLOAD_FILE")"
-# WORKLOAD_BASE="${WORKLOAD_NAME%.*}"
-# RESULTS_DIR="${EXPERIMENT_NAME}_${CONGESTION_CONTROL}_${WORKLOAD_BASE}"
-# MODE=central P4_PROGRAM=examples/qlrouting/fwd_build/fwd.json EXPERIMENT_NAME=${EXPERIMENT_NAME} CONGESTION_CONTROL=${CONGESTION_CONTROL} WORKLOAD_FILE=${WORKLOAD_FILE} EDGES="0,1;0,2;1,2" HOSTS="1,1,1" SWITCHES=3 DAGS="0:1-0,2-0,2-1;1:0-1,0-2,2-1;2:0-1,0-2,1-2" END=10 bash run_experiment.sh
-# python3 plot.py results/${RESULTS_DIR} figures/${RESULTS_DIR}
-# done
-
 chmod -R 777 figures
 
